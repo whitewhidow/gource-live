7a5cb9c3c7b7a47380c1c3249c683f07b567c9a2
Janos Gyerik <info@janosgyerik.com>
2013-05-11 10:51:45 +0200

    moved REMOTE and BRANCH parameters to the top and basic cleaning

--- git-live.sh
+++ git-live.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
-#IFS='\n'
-TAB='\t'
+
+REMOTE=origin
+BRANCH=master
 
 LASTCHECKED=`date +"%s"`
 
 while true
 do
-#    IGNORED=`git svn rebase`
-  PREFIXES=`git log origin/Development --since=$LASTCHECKED --reverse --max-count=1 --pretty=format:"%H|%at|%an|"`
+    PREFIXES=`git log $REMOTE/$BRANCH --since=$LASTCHECKED --reverse --max-count=1 --pretty=format:"%H|%at|%an|"`
     #PREFIXES=`git log --reverse --max-count=1 --pretty=format:"%H|%at|%an|"`
     LASTCHECKED=`date +"%s"`
     for i in $PREFIXES
     do
         SHA=`echo $i | cut -d '|' -f 1`
         PREFIX=`echo $i | cut -d '|' -f 2-3`
-        SUFFIXES=`git show origin/Development $SHA --pretty=format:"" --name-status | sed "s/\t/|/g"`
-#      SUFFIXES=`echo $SUFFIXES| sed "s/\t/|/g"`
-
+        SUFFIXES=`git show $REMOTE/$BRANCH $SHA --pretty=format:"" --name-status | sed "s/\t/|/g"`
+        # SUFFIXES=`echo $SUFFIXES| sed "s/\t/|/g"`
 
         for i in $SUFFIXES
         do
-            SUFFIX=`echo $i| sed "s/\t/|/g"| sed "s/ //g"`
+            SUFFIX=`echo $i| sed "s/\t/|/g" | sed "s/ //g"`
             echo ""$PREFIX"|"$SUFFIX
         done
     done
-	git fetch origin  > /dev/null 2>&1
+	git fetch $REMOTE  > /dev/null 2>&1
     sleep 5
 done
