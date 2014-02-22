736f578d40c1cf9737c91909ae632b7f5042f803
Janos Gyerik <janos@xw8400>
2014-02-22 12:05:15 +0100

    added usage msg to bzr feeder

--- feeders/feeder-bzr.sh
+++ feeders/feeder-bzr.sh
@@ -1,14 +1,18 @@
 #!/bin/sh -e
-#
-# TODO: rewrite using Python and bzrlib
 
-INTERVAL=$1; shift
+test $# -ge 1 || {
+    echo "usage: $0 INTERVAL [START_REVNO]"
+    exit 1
+}
 
-test "$1" && REVNO=$1 || { REVNO=$(bzr revno); test "$REVNO" -gt 10 && REVNO=$(bzr revno -rlast:10) || REVNO=1; }
+INTERVAL=$1
+START_REVNO=$2
+
+test "$START_REVNO" && REVNO=$START_REVNO || { REVNO=$(bzr revno); test "$REVNO" -gt 10 && REVNO=$(bzr revno -rlast:10) || REVNO=1; }
 
 while true
 do
-    for REVNO in $(bzr log --forward --line -r $REVNO.. | tail +2 | sed -e 's/:.*//')
+    for REVNO in $(bzr log --forward --line -r $REVNO.. | tail -n +2 | sed -e 's/:.*//')
     do
         AUTHOR=$(bzr log -r $REVNO | sed -ne 's/^committer: \([^<]*\).*/\1/p')
         TIMESTAMP=$(bzr log -r $REVNO | sed -ne 's/^timestamp: ... \([0-9 :-]*\) +.*/\1/p' | python -c 'from datetime import datetime as dt; import sys; ts = sys.stdin.readline().strip(); print dt.strftime(dt.strptime(ts, "%Y-%m-%d %H:%M:%S"), "%s")')
