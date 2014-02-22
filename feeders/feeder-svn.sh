82544842ebd2a01c13b17e1d71eca32acf2f2e02
Janos Gyerik <janos@xw8400>
2014-02-22 12:34:17 +0100

    added usage msg to svn feeder and fixed several bugs

--- feeders/feeder-svn.sh
+++ feeders/feeder-svn.sh
@@ -1,13 +1,18 @@
-#!/bin/bash -e
-#
+#!/bin/sh -e
 
-INTERVAL=$1; shift
+test $# -ge 1 || {
+    echo "usage: $0 INTERVAL [START_REVNO]"
+    exit 1
+}
 
-test "$1" && REVNO=$1 || { REVNO=$(svn info | sed -ne 's/^Revision: //p'); test "$REVNO" -gt 10 && REVNO=$((REVNO-10)) || REVNO=1; }
+INTERVAL=$1
+START_REVNO=$2
+
+test "$START_REVNO" && REVNO=$START_REVNO || REVNO=$(svn log -q -l 10 | sed -ne 's/^r\([0-9][0-9]*\).*/\1/p' | tail -n 1)
 
 while true
 do
-    for REVNO in $(svn log -qr$REVNO:HEAD | sed -ne 's/^r\([0-9][0-9]*\) \| .*/\1/p' | tail +2)
+    for REVNO in $(svn log -qr$REVNO:HEAD | sed -ne 's/^r\([0-9][0-9]*\).*/\1/p' | tail -n +2)
     do
         AUTHOR=$(svn log -qr $REVNO | sed -ne 2p | cut -f2 -d\| | sed -e 's/^ *//' -e 's/ *$//')
         TIMESTAMP=$(svn log -qr $REVNO | sed -ne 2p | cut -f3 -d\| | sed -e 's/^ \([0-9 :-]*\) .*/\1/' | python -c 'from datetime import datetime as dt; import sys; ts = sys.stdin.readline().strip(); print dt.strftime(dt.strptime(ts, "%Y-%m-%d %H:%M:%S"), "%s")')
